8a50418ceca45dec3e9716841af5c5ea181eb818
Ryan Gildea <ryan.gildea@privy.com>
2020-05-18 15:57:26 -0400

    so many changes

--- android/install.sh
+++ android/install.sh
@@ -3,12 +3,12 @@
 # Android
 #
 # This installs Android studio from the stable channel
-echo "› brew cask install android-studio"
-brew cask install android-studio
+#echo "› brew cask install android-studio"
+#brew cask install android-studio
 
 pwd
-ls $ZSH/android/path.zsh
-source $ZSH/android/path.zsh
+#ls $ZSH/android/path.zsh
+#source $ZSH/android/path.zsh
 
-echo "› sdkmanager --update"
-$ANDROID_HOME/tools/bin/sdkmanager --update
+#echo "› sdkmanager --update"
+#$ANDROID_HOME/tools/bin/sdkmanager --update
--- system/env.zsh
+++ system/env.zsh
@@ -1,3 +1,4 @@
-export EDITOR='code'
+#export EDITOR='code'
+export EDITOR='vim'
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
