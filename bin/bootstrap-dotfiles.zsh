6ab513e88effa1144d7aa5f18db10d116d7a18e0
Ryan Gildea <ryangildea@gmail.com>
2022-06-08 12:10:46 -0400

    updates to bootstrap script

--- bin/bootstrap-dotfiles.zsh
+++ bin/bootstrap-dotfiles.zsh
@@ -1,21 +1,23 @@
 #!/bin/zsh
 
 set -e
+DRY_RUN=false
+VERBOSE=false
 
-while getopts 'lha:' OPTION; do
+function output {
+ if [[ $VERBOSE = true ]]; then echo $1; fi
+}
+
+while getopts 'dv' OPTION; do
   case "$OPTION" in
     d)
-      echo "d used"
+      DRY_RUN=true
       ;;
     v)
-      echo "v used"
-      ;;
-    a)
-      avalue="$OPTARG"
-      echo "The value provided is $OPTARG"
+      VERBOSE=true
       ;;
     ?)
-      echo "script usage: $(basename \$0) [-l] [-h] [-a somevalue]" >&2
+      echo "script usage: $(basename \$0) [-d] [-v] [-a somevalue]" >&2
       exit 1
       ;;
   esac
@@ -25,42 +27,70 @@ shift "$(($OPTIND -1))"
 # install homebrew
 if [[  $+commands[brew] ]]
 then
-  echo "hombrew already installed to $(type -p "brew"), skipping."
+#  output "homebrew already installed to $(type -p "brew"), skipping."
+  output "oof"
 else
-  echo "installing homebrew..."
+  output "installing homebrew..."
   # do it here
 fi
 
 # install oh-my-zsh
 if [ -d $HOME/.oh-my-zsh ]
 then
-  echo "oh-my-zsh already installed, skipping."
+  output "oh-my-zsh already installed...skipping."
 else
-  echo "installing oh-my-zsh"
-  # do it here
-  #
+  output "installing oh-my-zsh"
+  if [[ ! $DRY_RUN = true ]]
+  then
+    # do it here
+  fi
 fi
 
 declare -A ZSH_CUSTOM_PLUGINS
 
-ZSH_CUSTOM_PLUGINS=$ZSH_CUSTOM/plugins
+ZSH_CUSTOM_PLUGINS_DIR=~/.oh-my-zsh/custom/plugins
 
-ZSH_CUSTOM_PLUGINS=(
-  [dotbare]="git clone https://github.com/kazhala/dotbare.git $ZSH_CUSTOM/plugins/dotbare"
-  [zsh-autocomplete]="git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
-  [zsh-syntax-highlighting]=""
-  )
+ZSH_CUSTOM_PLUGINS[dotbare]="git clone https://github.com/kazhala/dotbare.git $ZSH_CUSTOM_PLUGINS_DIR/dotbare"
+ZSH_CUSTOM_PLUGINS[dotbare]="git clone https://github.com/kazhala/dotbare.git $ZSH_CUSTOM_PLUGINS_DIR/dotbare"
+
+ZSH_CUSTOM_PLUGINS[zsh-autocomplete]="git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM_PLUGINS_DIR/zsh-syntax-highlighting"
+
+ZSH_CUSTOM_PLUGINS[zsh-syntax-highlighting]=""
+
+echo $ZSH_CUSTOM_PLUGINS
+
 
 
 # install custom zsh plugins
-if [ -d $ZSH_CUSTOM/plugins/dotbare ]
+for plugin plugin_install_cmd in ${(kv)ZSH_CUSTOM_PLUGINS}; do
+  echo $ZSH_CUSTOM_PLUGINS_DIR/$plugin
+  if [ -d $ZSH_CUSTOM_PLUGINS_DIR/$plugin ]
+  then
+    output "${plugin} already installed...skipping"
+  else 
+    if [[ ! $DRY_RUN = true ]]
+    then
+      # install plugin
+      output "evaluating ${plugin_install_cmd}"
+      eval ${plugin_install_cmd}
+    fi
+  fi
+done
+
+# Update homebrew
+output "brew update"
+brew update
+
+# install brew packages
+output "brew bundle"
+brew bundle
+
+# set up vim
+# - install vim-plug
+if [[ $DRY_RUN = false && ! -e $HOME/.vim/autoload/plug.vim ]]
 then
-  echo "dotbare already installed...skipping"
-else
-  echo "installing oh-my-zsh plugin 'dotbare'..."
-  # do it here
+    output "installing vim-plug..."
+    # curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-# Upgrade homebrew
-echo "brew update"
-# brew update
+echo "DONE!"
