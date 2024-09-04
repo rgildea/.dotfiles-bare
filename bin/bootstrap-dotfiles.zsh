2b025eaa08bf87ab403eb5aa57b2e0065d005916
Ryan Gildea <ryangildea@gmail.com>
2024-09-04 09:57:04 -0400

    fix issues

--- bin/bootstrap-dotfiles.zsh
+++ bin/bootstrap-dotfiles.zsh
@@ -58,7 +58,7 @@ fi
 
 
 # install dotbare temp installation -- need it to install dotfiles
-DOTBARE_TMP_DIR=$HOME/.dotbare-tmp
+DOTBARE_TMP_DIR=$HOME/.dotbare-tmp-$( date +%s )
 rm -rf $DOTBARE_TMP_DIR
 if [[ -x $(which dotbare) ]]
 then
