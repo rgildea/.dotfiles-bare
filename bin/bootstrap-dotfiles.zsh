7d26c6e48a1d4107e995f1537bbc7e5830572008
Ryan Gildea <ryangildea@gmail.com>
2024-09-05 10:30:20 -0400

    more worky

--- bin/bootstrap-dotfiles.zsh
+++ bin/bootstrap-dotfiles.zsh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+echo "Starting bootstrap script..."
 
 # Close any open System Preferences panes, to prevent them from overriding
 # settings we’re about to change
@@ -6,17 +7,16 @@ osascript -e 'tell application "System Preferences" to quit'
 
 # Ask for the administrator password upfront
 sudo -v
-
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 set -e
 DRY_RUN=false
-VERBOSE=false
+VERBOSE=true
 
-function output {
- if (( $VERBOSE = true )); then echo $1; fi
-}
+# function output {
+#  if (( $VERBOSE  = true )); then echo $1; fi
+# }
 
 while getopts 'dv' OPTION; do
   case "$OPTION" in
@@ -25,6 +25,7 @@ while getopts 'dv' OPTION; do
       ;;
     v)
       VERBOSE=true
+      echo "verbose mode on"
       ;;
     ?)
       echo "script usage: $(basename \$0) [-d] [-v] [-a somevalue]" >&2
@@ -37,53 +38,56 @@ shift "$(($OPTIND -1))"
 # install homebrew
 if [[ -x $(which brew) ]]
 then
-  output "homebrew already installed to $(type -p "brew"), skipping."
+  echo "homebrew already installed to $(type -p "brew"), skipping."
 else
-  output "installing homebrew..."
+
+  echo "installing homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zshrc
   eval "$(/opt/homebrew/bin/brew shellenv)"
-  output "done installing homebrew"
+  echo "done installing homebrew"
 fi
 
 
 # install xcode dev tools
 if [[ -n "$(xcode-select -p)" ]]
 then
-  output "xcode dev tools already installed...skipping."
+  echo "xcode dev tools already installed...skipping."
 else
-  output "installing xcode-select..."
+  echo "installing xcode-select..."
   xcode-select --install || true
 fi
 
-
+echo "installing 1Password..."
 # install one-password ssh integration
-OP_BIN=$(which op)
+echo "installing 1Password..."
+
 OP_SCRATCH_DIR=$HOME/.1password
+
 
 if [[ -x $(which op) ]]
 then
-  output "1Password already installed..skipping."
+  echo "1Password already installed..skipping."
 else
-  output "Installing 1Password..."
-  brew  install --cask 1password 1password-cli
+  echo "Installing 1Password..."
+  brew  install 1password 1password-cli
   
-  # create a scratch directory for 1password
-  [[ ! -d $OP_SCRATCH_DIR ]] && mkdir -p $OP_SCRATCH_DIR
+  # # create a scratch directory for 1password
+  # [[ ! -d $OP_SCRATCH_DIR ]] && mkdir -p $OP_SCRATCH_DIR
   
-  ln -s ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
-  SSH_AUTH_SOCK=~/.1password/agent.sock
+  # ln -s ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
+  # SSH_AUTH_SOCK=~/.1password/agent.sock
 
   # sign in to 1password
-  input "Please sign in to 1Password and press enter to continue..."
+  read -p "Please sign in to 1Password and press enter to continue..."
   open -a "1Password"
-  input "Press enter to continue..."
+  read -p "Press enter to continue..."
   
   # test the 1password ssh agent
   if ssh -T git@github.com; then
-    output "1Password SSH Agent is working"
+    echo "1Password SSH Agent is working"
   else
-    output "1Password SSH Agent is not working"
+    echo "1Password SSH Agent is not working"
     exit 1
   fi
 fi
@@ -94,9 +98,9 @@ DOTBARE_TMP_DIR=$DOTBARE_FILE_ROOT$( date +%s )
 rm -rf $HOME/.dotbare-tmp-* || true
 if [[ -x $(which dotbare) ]]
 then
-    output "dotbare already installed...skipping."
+    echo "dotbare already installed...skipping."
 else
-    output "installing dotbare..."
+    echo "installing dotbare..."
     git clone https://github.com/kazhala/dotbare.git $DOTBARE_TMP_DIR
     source $DOTBARE_TMP_DIR/dotbare.plugin.zsh
 fi
@@ -104,35 +108,35 @@ fi
 # install dotfiles
 if [[ -d $HOME/.cfg ]]
 then
-    output "dotfiles already installed...skipping."
+    echo "dotfiles already installed...skipping."
 else
-    output "installing dotfiles..."
+    echo "installing dotfiles..."
     dotbare finit -u https://github.com/rgildea/.dotfiles-bare.git
     dotbare submodule update --init --recursive
 fi
 
 # Update homebrew
-output "brew update"
+echo "brew update"
 brew update
 
 # install brew packages
-output "brew bundle"
+echo "brew bundle"
 brew bundle
 
 # install oh-my-zsh
 if [[ -d $HOME/.oh-my-zsh ]]
 then
-  output "oh-my-zsh already installed...skipping."
+  echo "oh-my-zsh already installed...skipping."
 else
-  output "installing oh-my-zsh"
+  echo "installing oh-my-zsh"
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 # configure asdf
 if [[ -x $(which asdf) ]]
-then output "asdf already setup...skipping."
+then echo "asdf already setup...skipping."
 else
-  output "configuring asdf..."
+  echo "configuring asdf..."
   asdf plugin add nodejs
   asdf plugin add python
   asdf plugin add ruby
@@ -146,20 +150,20 @@ fi
 # install janus for vim
 if [[ -d $HOME/.vim/janus ]]
 then
-  output "janus already installed...skipping."
+  echo "janus already installed...skipping."
 else
-  output "installing janus..."
+  echo "installing janus..."
   curl -L https://bit.ly/janus-bootstrap | bash
 fi
 
 # cleanup
-output "cleaning up..."
+echo "cleaning up..."
 rm -rf $DOTBARE_TMP_DIR
 brew cleanup
 
 # reload zsh config
 source $HOME/.zshrc
-output "DONE! opening iTerm..."
+echo "DONE! opening iTerm..."
 
 # open iTerm and set up the prompt
 open -a iTerm --args -e "p10k configure"
